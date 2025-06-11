bf7136b2042cbf780e806981ce10639c56eddc44
sarah_syed <sarahsyed2009@gmail.com>
2025-06-11 17:37:43 -0500

    First Commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var button = document.querySelector(".show-cat");
+var cat = document.querySelector(".cat");
+
+button.addEventListener("click", function(){
+   //console.log("It's working");
+   cat.classList.add("show");
+}
+)
+
+
+
+
